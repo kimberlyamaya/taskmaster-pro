d75e720be77b002524893b1cc17a2d972cf28afb
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-01 15:39:34 -0700

    customize/improve user expierence

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,8 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  console.log(taskEl);
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -81,16 +83,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active");
   },
   update: function() {
     var tempArr = [];
@@ -133,12 +137,13 @@ $("#trash").droppable({
   drop: function(event, ui) {
     // remove dragged element from the dom
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -161,7 +166,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -275,6 +280,12 @@ $(".list-group").on("change", "input[type='text']", function() {
     $(this).replaceWith(taskSpan);
     auditTask($(taskSpan).closest(".list-group-item"));
 });
+
+setInterval(function() {
+  $(".card .list-group-item") .each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
